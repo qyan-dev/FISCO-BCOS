2a8596eb184ae6ddbde325aee03e480f79f02bc5
HaoXuan40404 <444649358@qq.com>
2019-06-19 21:07:20 +0800

    precompile with rust

---
+++ libprecompiled/precompile.h
@@ -0,0 +1,13 @@
+#if __cplusplus
+extern "C" {
+#endif
+// char CallStringTest(int _a);
+// char * returnCtring(int _a);
+// char * Ctring2string(char *_a);
+// char * constCtring2string(char *a);
+// void free_c_string(char *_a);
+char * knowledge_verify(char *a, char *b, char *c ,char *d, char *e);
+
+#if __cplusplus
+}
+#endif
